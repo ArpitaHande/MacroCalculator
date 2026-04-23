0e2067a5ec6f415187346e67b594b68099bee50b
aditi-lakhe <aditi.n.lakhe@cumminscollege.in>
2026-04-23 21:39:45 +0530

    Input user deatils about age ,gender,weight,height,goal and calculate macros

--- main.py
+++ main.py
@@ -21,3 +21,36 @@ def Load_Data():
     return df
 
 Load_Data()
+
+# User_Inputs
+st.sidebar.header("👤 Your Profile")
+age = st.sidebar.slider("Age", min_value=10, max_value=100, value=25)
+gender = st.sidebar.selectbox("Gender", ["Male", "Female"])
+weight = st.sidebar.slider("Weight (kg)", 30, 200, 70)
+height = st.sidebar.slider("Height (cm)", 100, 250, 170)
+goal = st.sidebar.selectbox("Goal", ["Weight Loss", "Maintain", "Weight Gain"])
+
+#Macro Calculation Logic
+def calculate_macros(weight, height, age, gender, goal):
+    # Calculate BMR
+    if gender == "Male":
+        bmr = 10 * weight + 6.25 * height - 5 * age + 5
+    else:
+        bmr = 10 * weight + 6.25 * height - 5 * age - 161
+    
+    # TDEE Calculation based on goal
+    if goal == "Weight Loss":
+        tdee = bmr * 1.2 - 500
+    elif goal == "Weight Gain":
+        tdee = bmr * 1.2 + 500
+    else:
+        tdee = bmr * 1.2
+        
+    # Standard Macro Split: 50% Carbs, 25% Protein, 25% Fat
+    carbs = (tdee * 0.5) / 4
+    protein = (tdee * 0.25) / 4
+    fats = (tdee * 0.25) / 9
+    
+    return round(tdee ,3), round(carbs,3), round(protein,3), round(fats,3)
+
+calories, carbs, protein, facts = calculate_macros(weight, height, age, gender, goal)
